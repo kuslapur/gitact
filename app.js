1781b8c9a8fe3d464a2738243393059fa142e037
bkuslapur <bkuslapur@gmail.com>
2026-03-24 23:51:44 +0530

    3 tier app'

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Hello from EKS 🚀");
-});
-
-app.listen(3000, () => console.log("Running on 3000"));
